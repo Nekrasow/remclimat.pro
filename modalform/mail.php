3b284fc5f033b295be2a29a5399c049e371a6f1f
redhat <redhat.ua@gmail.com>
2016-09-27 00:09:24 +0300

    Update mail.php

--- modalform/mail.php
+++ modalform/mail.php
@@ -1,38 +1,8 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST['uphone'])){
-    echo '<p class="fail">Ошибка. Вы заполнили не все обязательные поля!</p>';
-  } else{
-    if (isset($_POST['uphone'])) {
-      $uphone = strip_tags($_POST['uphone']);
-      $uphoneFieldset = "<b>Телефон:</b>";
-    }
-
-    if (isset($_POST['formInfo'])) {
-      $formInfo = strip_tags($_POST['formInfo']);
-      $formInfoFieldset = "<b>Тема:</b>";
-    }
-
-    $to = "1986Nekrasow@gmail.com" . ","; /*Укажите адрес, на который должно приходить письмо*/
-    $to .= "Myskat_06@mail.ru";
-    $sendfrom = "remclimat.pro@yandex.ru"; /*Укажите адрес, с которого будет приходить письмо, можно не настоящий, нужно для формирования заголовка письма*/
-    $headers  = "From: " . strip_tags($sendfrom) . "\r\n";
-    $headers .= "Reply-To: ". strip_tags($sendfrom) . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-    $headers .= "Content-Transfer-Encoding: 8bit \r\n";
-    $subject = "$formInfo";
-    $message = "$uphoneFieldset $uphone<br>
-                $formInfoFieldset $formInfo";
-
-    $send = mail ($to, $subject, $message, $headers);
-        if ($send == 'true') {
-            echo '<p class="success">Спасибо за отправку вашего сообщения!</p>';
-        } else {
-          echo '<p class="fail"><b>Ошибка. Сообщение не отправлено!</b></p>';
-        }
-  }
+include_once 'mailer.class.php';
+$location = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'];
+if ($_SERVER['REQUEST_METHOD'] == "POST" && !empty($_POST)) {
+    (new Mailer($_POST));
 } else {
-  header ("Location: http://remclimat.pro/"); // главная страница вашего лендинга
+    header("Location: $location");
 }
-?>
